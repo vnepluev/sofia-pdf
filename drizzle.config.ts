384d3917cbbc2ee89da3309a3510c336d3ff43d1
Valeriy V Nepluev <greenet2007@gmail.com>
2025-03-12 03:00:01 +0300

    feat: configure Drizzle ORM

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,16 @@
-import 'dotenv/config';
-import { defineConfig } from 'drizzle-kit';
+import { defineConfig } from 'drizzle-kit'
+import dotenv from 'dotenv'
+import path from 'path'
+
+const envFile = process.env.NODE_ENV === 'development' ? '.env.development' : '.env.production'
+
+dotenv.config({ path: path.resolve(__dirname, envFile) })
 
 export default defineConfig({
-  out: './sofia-pdf-app/shared/api/db/migrations',
-  schema: './sofia-pdf-app/shared/api/db/schema.ts',
+  out: './drizzle/migrations',
+  schema: './server/db/schemas/*.schema.ts',
   dialect: 'sqlite',
   dbCredentials: {
     url: process.env.DB_FILE_NAME!,
   },
-});
+})
--- server/db/index.ts
+++ server/db/index.ts
@@ -5,6 +5,10 @@ import 'dotenv/config'
 import { drizzle } from 'drizzle-orm/bun-sqlite'
 import { Database } from 'bun:sqlite'
 
-const sqlite = new Database(process.env.DB_FILE_NAME!)
+// если файла БД нет, создаем его
+//https://bun.sh/docs/api/sqlite
+const sqlite = new Database(process.env.DB_FILE_NAME!, { create: true })
 
 export const db = drizzle(sqlite)
+
+export * from './schemas/users.schema'
